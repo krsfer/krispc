688d983986fb77babfbf8a2d6662df0594143de9
krsfer <archer.chris@gmail.com>
2023-09-28 12:28:20 +0200

    Added chat requirements

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -70,10 +70,22 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "channels",
     "hello",
     "chat",
     "addthem",
 ]
+
+ASGI_APPLICATION = 'gettingstarted.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG':  {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
