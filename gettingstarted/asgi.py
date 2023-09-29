0f5d36bf130f84ed79138d53c55fbcbd495b4646
krsfer <archer.chris@gmail.com>
2023-09-29 22:34:00 +0200

    Simplify select ws wss. Tweak and untweak asgi.py

--- gettingstarted/asgi.py
+++ gettingstarted/asgi.py
@@ -7,6 +7,35 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+# import os
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+#
+# import chat.routing
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+# django_asgi_app = get_asgi_application()
+#
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+#
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+#
+#
+# application = ProtocolTypeRouter(
+#     {
+#         "http": django_asgi_app,
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(
+#                 URLRouter(
+#                     chat.routing.websocket_urlpatterns
+#                 )
+#             )
+#         ),
+#     }
+# )
+
 import os
 
 from channels.auth import AuthMiddlewareStack
@@ -19,7 +48,7 @@ import chat.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 application = ProtocolTypeRouter({
-    "http":      get_asgi_application(),
+    "http": get_asgi_application(),
     "websocket":
         AllowedHostsOriginValidator(
             AuthMiddlewareStack(
