5fcda2522371bb1e7ffed14aa588e48b4997c2d0
krsfer <archer.chris@gmail.com>
2023-10-01 10:45:35 +0200

    Remove pprint from views.py

--- krispc/views.py
+++ krispc/views.py
@@ -1,15 +1,9 @@
-from pprint import pprint
-
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.utils.translation import get_language
 from django.views.decorators.http import require_GET
 from django.views.generic import TemplateView
-from django.utils.translation import get_language
 
 from _main import settings
-from _main.settings import DEBUG
-
-
 from krispc import colophon, forms, lst_products, lst_villes, marques
 
 
@@ -17,9 +11,7 @@ from krispc import colophon, forms, lst_products, lst_villes, marques
 class IndexKPageView(TemplateView):
     template_name = "_index.html"
 
-
     def render_to_response(self, context, **response_kwargs):
-
         # request.META.get('HTTP_REFERER')
         response = super(IndexKPageView, self).render_to_response(
             {
@@ -35,7 +27,7 @@ class IndexKPageView(TemplateView):
             **response_kwargs,
         )
 
-        pprint(response.__dict__)
+        # pprint(response.__dict__)
 
         # return render(request, '_index.html', {"prods": products.data()})
         # response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
@@ -46,9 +38,9 @@ class IndexKPageView(TemplateView):
 def favicon(request) -> HttpResponse:
     return HttpResponse(
         (
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
-            + '<text y=".9em" font-size="90">🦊</text>'
-            + "</svg>"
+                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
+                + '<text y=".9em" font-size="90">🦊</text>'
+                + "</svg>"
         ),
         content_type="image/svg+xml",
     )
