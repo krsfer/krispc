6b9f09a09358a84a62d6af29c3a5093f8d732579
krsfer <archer.chris@gmail.com>
2023-10-07 09:52:54 +0200

    Styled addthem

--- addthem/static/js/code.js
+++ addthem/static/js/code.js
@@ -5,11 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let count = 0;
     let max = 10;
     let total_time = 0;
+
+    let larve = [];
+
     const logTable = document.getElementById("logTable");
     const userInput = document.getElementById("userInput");
     const question = document.getElementById("question");
-
     const incorrect = document.getElementById("incorrect");
+
+    const data = document.getElementById("data");
 
     let startTime = Date.now();
 
@@ -17,21 +21,38 @@ document.addEventListener("DOMContentLoaded", () => {
     function displayNumbers() {
         const numbers = generateRandomNumbers();
         numbers.sort((a, b) => a - b);
-        console.log("numbers", numbers);
+        // console.log("numbers", numbers);
         question.innerHTML = numbers.join(" ");
     }
 
-    function getUserInput() {
-        return userInput.value.trim();
-    }
-
     // Load from local storage if available
-    const savedLog = localStorage.getItem('log');
-    if (savedLog) {
-        logTable.innerHTML = savedLog;
-        console.log("Loaded from local storage");
-        console.log(savedLog);
+    // const savedLog = localStorage.getItem('log');
+    // if (savedLog) {
+    //     logTable.innerHTML = savedLog;
+    //     // console.log("Loaded from local storage");
+    //     // console.log(savedLog);
+    // }
+
+    let savedLarve = localStorage.getItem('larve');
+
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    if (savedLarve) {
+        try {
+            let str = JSON.parse(savedLarve);
+            if (str !== null) {
+
+                data.innerHTML = makeBS5str(str);
+
+                larve = str;
+            }
+        } catch {
+            console.log("failed to parse");
+            console.log(savedLarve);
+        }
+    } else {
+        console.log("does not exist");
     }
+    console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
     userInput.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
@@ -46,7 +67,25 @@ document.addEventListener("DOMContentLoaded", () => {
         const guess = userInput.value.trim();
 
 
-        if (guess === "q" || guess === "s" || /^[0-9a-z]+$/.test(guess)) {
+        function processGuess(numbers, elapsedTime) {
+            const sum = numbers.reduce((acc, val) => acc + val, 0);
+
+            let int_guess = parseInt(guess);
+
+            let is_correct = int_guess === sum;
+
+            if (is_correct) {
+                incorrect.innerHTML = "";
+            } else {
+                incorrect.innerHTML = sum.toString();
+            }
+
+            addLog(count + 1, numbers, int_guess, is_correct, elapsedTime, new Date().toISOString(), sum);
+
+            count++;
+        }
+
+        if (guess === "q") {
             const endTime = Date.now();
             const elapsedTime = (endTime - startTime) / 1000;
             total_time += elapsedTime;
@@ -65,20 +104,50 @@ document.addEventListener("DOMContentLoaded", () => {
                 return;
             }
 
-            const sum = numbers.reduce((acc, val) => acc + val, 0);
+            processGuess(numbers, elapsedTime);
 
-            let is_correct = false;
-            is_correct = parseInt(guess) === sum;
+        } else if (guess === "s") {
+            const endTime = Date.now();
+            const elapsedTime = (endTime - startTime) / 1000;
+            total_time += elapsedTime;
 
-            if (is_correct) {
-                incorrect.innerHTML = "";
-            } else {
-                incorrect.innerHTML = sum;
+            let numbers = question.innerHTML.split(" ");
+            numbers = numbers.map((val) => parseInt(val));
+
+            if (guess === "q") {
+                // Quit the session
+                return;
             }
 
-            addLog(count + 1, numbers, guess, is_correct, elapsedTime, new Date().toISOString(), sum);
+            if (guess === "s") {
+                // Skip the question
+                count++;
+                return;
+            }
 
-            count++;
+            processGuess(numbers, elapsedTime);
+
+        } else if (/^[0-9a-z]+$/.test(guess)) {
+            const endTime = Date.now();
+            const elapsedTime = (endTime - startTime) / 1000;
+            total_time += elapsedTime;
+
+            let numbers = question.innerHTML.split(" ");
+            numbers = numbers.map((val) => parseInt(val));
+
+            if (guess === "q") {
+                // Quit the session
+                return;
+            }
+
+            if (guess === "s") {
+                // Skip the question
+                count++;
+                return;
+            }
+
+            processGuess(numbers, elapsedTime);
+
         } else {
             alert("Invalid input. Enter only characters 0-9, a-z, or space.");
         }
@@ -89,8 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.clearLog = () => {
         localStorage.clear();
-        logTable.innerHTML = "<tr><th>Question</th><th>Numbers</th><th>Guess</th><th>Elapsed" +
-            " Time(s)</th><th><span class='hidden'>Date</span></th></tr>";
+        data.innerHTML = "";
     };
 
     window.newSession = () => {
@@ -104,23 +172,74 @@ document.addEventListener("DOMContentLoaded", () => {
         return Array.from({length}, () => Math.floor(Math.random() * 9) + 1);
     }
 
-    function addLog(question, numbers, guess, is_correct, elapsedTime, date, sum) {
-        let couleur = "";
+    function makeBS5str(larve_str) {
+        let start_str = "<div class=\"container border\">";
 
-        console.log("is_correct", is_correct);
+        let content = "";
+        for (let i = 0; i < larve_str.length; i++) {
+            content += "<div class=\"row\">";
+
+            for(let key in larve_str[i]) {
+                if(key === 'date')
+                    continue
+                if(key === 'numbers') {
+                    console.log(key);
+                    let num_str = larve_str[i][key].join(" ");
+                    content += `<div class="col px-0">${num_str}</div>`;
+                }
+                else
+                    content += `<div class="col px-0">${larve_str[i][key]}</div>`
+            }
+            content += "</div>";
+        }
+
+        let end_str = "</div>"
+
+        let res;
+        res = start_str + content + end_str;
+
+        return res;
+    }
+
+    function addLog(nth, numbers, guess, is_correct, elapsedTime, date, sum) {
+        let couleur;
+
+        let json;
+        json = {
+                nth: nth,
+                numbers: numbers,
+                guess: guess,
+                is_correct: is_correct,
+                elapsedTime: elapsedTime,
+                date: date,
+                sum: sum
+        }
+        console.log(typeof larve);
+        larve.push(json);
+
+        localStorage.removeItem('larve');
+        let larve_str = JSON.stringify(larve);
+        localStorage.setItem('larve', larve_str);
+
+
+        data.innerHTML = makeBS5str(larve);
+
+
+
+        // console.log("is_correct", is_correct);
 
         couleur = is_correct ? "#DAF9EA" : "#F9E0DA";
 
-        console.log("couleur", couleur);
+        // console.log("couleur", couleur);
 
-        const newRow = logTable.insertRow(-1);
-        newRow.innerHTML = `<td bgcolor=${couleur}>${question}</td><td>${numbers.join(" ")}</td><td>${guess}</td><td>${elapsedTime.toFixed(2)}</td><td><span class="hidden">{date}</span></td>`;
-        localStorage.setItem('log', logTable.innerHTML);
+        // const newRow = logTable.insertRow(-1);
+        // newRow.innerHTML = `<td bgcolor=${couleur}>${nth}</td><td>${numbers.join(" ")}</td><td>${guess}</td><td>${elapsedTime.toFixed(2)}</td><td><span class="hidden">{date}</span></td>`;
+        // localStorage.setItem('log', logTable.innerHTML);
 
         if (parseInt(guess) === sum) {
             console.log("Correct");
         } else {
-            console.log(`The sum is ${sum}`);
+            console.log(`Incorrect : The sum is ${sum}`);
         }
     }
 
--- wat/static/js/mymap.js
+++ wat/static/js/mymap.js
@@ -13,15 +13,12 @@
         zoom: 14
     });
 
-    map.addControl(
-        new mapboxgl.GeolocateControl({
+    const geolocate = new mapboxgl.GeolocateControl({
             positionOptions: {
                 enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserHeading: true
-        })
-    );
+            }, showAccuracyCircle: true, showUserHeading: true, trackUserLocation: true,
+        });
+        map.addControl(geolocate, 'top-right');
 
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
@@ -32,4 +29,89 @@
         console.log(coordsTxt);
     });
 
+    // Fired on each Geolocation API position update that returned as success
+        geolocate.on('geolocate', async (e) => {
+
+            /*
+                e is coords : Coordinates = eg.
+
+                accuracy : 20
+                altitude : null
+                altitudeAccuracy : null
+                heading : null
+                latitude : 43.6415469
+                longitude : 7.0108263
+                speed : null
+            */
+
+
+            const coords = e.coords;
+            let lonlat = [coords.longitude, coords.latitude];
+            // _blue_geojson_sphere.features[0].geometry.coordinates = lonlat;
+            // map.getSource('blue_source').setData(_blue_geojson_sphere);
+
+
+            // if (_state !== 1) {
+            //     lonlat[0] += 0.0001;
+            //     lonlat[1] += 0.0001;
+            // }
+
+            // let msg = {
+            //     "seq": seq, "lo": lonlat[0], "la": lonlat[1], "ts": new Date().getTime() / 3000
+            // };
+
+
+            // await do_chat(msg);
+
+
+            // await show_interRoute(lonlat, HOME_LatLon, true);
+
+            /*
+            console.log("FROM", FROM);
+            console.log("TO", TO);
+            // test if FROM and TO are defined
+
+            if (FROM != null && TO != null) {
+                console.log("FROM", FROM);
+                console.log("TO", TO);
+                // await show_interRoute(FROM, TO);
+            } else {
+                console.log("Eiher FROM or TO is null");
+                // await show_interRoute(FROM, BLUE_LatLon);
+            }*/
+
+
+            // blue_geojson_cir˛e.features[0].geometry.coordinates = [coords.longitude, coords.latitude];
+            // map.getSource('blue_point').setData(blue_geojson_circle);
+
+            // BLUE = [coords.longitude, coords.latitude];
+
+            // await do_chat();
+
+            // getRoute(false); // geolocate.on('geolocate', (e) => {...});
+
+        });
+
+        geolocate.on('trackuserlocationstart', () => {
+        });
+
+        geolocate.on('trackuserlocationend', () => {
+            /*const bounds = new mapboxgl.LngLatBounds(ROUTE[0], ROUTE[0]);
+
+            // Extend the 'LngLatBounds' to include every coordinate in the bounds result.
+            for (const coord of ROUTE) {
+                bounds.extend(coord);
+            }
+
+            map.fitBounds(bounds, {
+                padding: 100
+            });*/
+
+        });
+
+        geolocate.on('error', (e) => {
+            console.log(e.error);
+        });
+
 })();
+//
