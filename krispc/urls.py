d4bcc554f2f92b5a34fa27160cd5ebdc2259d3e0
krsfer <archer.chris@gmail.com>
2023-10-15 18:55:52 +0200

    reverting?

--- krispc/urls.py
+++ krispc/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from krispc.views import create_contact, favicon, IndexPageView
+from krispc.views import favicon, IndexPageView
 
 urlpatterns = [
     # path('change_language/', change_language, name='change_language'),
     # path("", IndexPageView.as_view(), name="index"),
     path("", IndexPageView.as_view(), name="_index"),
-    path("create/", create_contact, name="create_contact"),
     path("favicon.ico", favicon),
 ]
