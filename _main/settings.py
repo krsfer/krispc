6929dedc60f21afbd14fdc1363f1622675b7c2fc
krsfer <archer.chris@gmail.com>
2025-12-30 20:56:00 +0100

    fix(deploy): disable whitenoise file deletion for Vite dynamic imports

--- _main/settings.py
+++ _main/settings.py
@@ -270,7 +270,7 @@ STORAGES = {
 
 # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
 # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
-WHITENOISE_KEEP_ONLY_HASHED_FILES = True
+WHITENOISE_KEEP_ONLY_HASHED_FILES = False
 
 WHITENOISE_MANIFEST_STRICT = False
 
