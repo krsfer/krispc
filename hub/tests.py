d9aec21276d64f4e1037387d513586894bb0d35c
krsfer <archer.chris@gmail.com>
2026-01-02 00:15:00 +0100

    feat(hub): implement IndexView with bilingual context

--- hub/tests.py
+++ hub/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.utils.translation import activate
+from .views import IndexView
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_index_view_context_french(self):
+        activate('fr')
+        request = self.factory.get('/')
+        view = IndexView()
+        view.request = request
+        context = view.get_context_data()
+
+        self.assertEqual(context['page_title'], 'Christopher')
+        self.assertEqual(context['tagline'], 'Services et Outils Professionnels')
+        self.assertEqual(len(context['apps']), 2)
+        self.assertEqual(context['apps'][0]['name'], 'KrisPC')
+        self.assertEqual(context['apps'][1]['name'], 'Pdf2Cal')
+
+    def test_index_view_context_english(self):
+        activate('en')
+        request = self.factory.get('/')
+        view = IndexView()
+        view.request = request
+        context = view.get_context_data()
+
+        self.assertEqual(context['tagline'], 'Professional Services & Tools')
+        self.assertEqual(context['apps'][0]['button_text'], 'Visit')
--- hub/views.py
+++ hub/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.utils.translation import get_language
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "hub/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        is_french = get_language() == 'fr'
+
+        context['page_title'] = 'Christopher'
+        context['tagline'] = (
+            'Services et Outils Professionnels' if is_french
+            else 'Professional Services & Tools'
+        )
+
+        context['apps'] = [
+            {
+                'name': 'KrisPC',
+                'icon': '🖥️',
+                'description': (
+                    'Réparations informatiques professionnelles' if is_french
+                    else 'Professional computer repairs'
+                ),
+                'url': '/krispc/',
+                'button_text': 'Accéder' if is_french else 'Visit',
+            },
+            {
+                'name': 'Pdf2Cal',
+                'icon': '📅',
+                'description': (
+                    'Convertissez vos PDF en calendriers' if is_french
+                    else 'Convert your PDFs to calendars'
+                ),
+                'url': '/importpdf/',
+                'button_text': 'Accéder' if is_french else 'Visit',
+            }
+        ]
+
+        return context
+
+
+class PrivacyView(TemplateView):
+    template_name = "hub/privacy.html"
+
+
+class TermsView(TemplateView):
+    template_name = "hub/terms.html"
