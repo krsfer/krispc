3a131165b78fdbb892e4a602cce0ee36c5ff56f0
krsfer <archer.chris@gmail.com>
2026-01-21 22:11:42 +0100

    fix: remove chat app reference from asgi config

--- _main/asgi.py
+++ _main/asgi.py
@@ -7,35 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-# import os
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-#
-# import chat.routing
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-# django_asgi_app = get_asgi_application()
-#
-# from channels.auth import AuthMiddlewareStack
-# from channels.security.websocket import AllowedHostsOriginValidator
-#
-# # Initialize Django ASGI application early to ensure the AppRegistry
-# # is populated before importing code that may import ORM models.
-#
-#
-# application = ProtocolTypeRouter(
-#     {
-#         "http": django_asgi_app,
-#         "websocket": AllowedHostsOriginValidator(
-#             AuthMiddlewareStack(
-#                 URLRouter(
-#                     chat.routing.websocket_urlpatterns
-#                 )
-#             )
-#         ),
-#     }
-# )
-
 import os
 
 from channels.auth import AuthMiddlewareStack
@@ -43,18 +14,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-import chat.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_main.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket":
-        AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(
-                    chat.routing.websocket_urlpatterns
-                )
-            )
-        ),
+    # Websockets removed as chat app is deleted
 })
